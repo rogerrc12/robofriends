c6c1378505e846c0216a6373e7ab862e09ace151
Roger Rengifo <rogerreng.c@gmail.com>
2019-06-06 10:27:03 -0500

    Accesibility

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
   return (
     <div className="bg-light-green dib-ns br2 pa3 ma2 tc grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}`} alt="Robots" />
+      <img src={`https://robohash.org/${id}?size=200x200`} alt="Robot" />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,8 +4,10 @@ const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
+        aria-label="Search Robots"
         className="pa3 ba b--green bg-lightest-blue tc"
         type="search"
+        id="searchBox"
         placeholder="search your robot"
         onChange={searchChange}
       />
