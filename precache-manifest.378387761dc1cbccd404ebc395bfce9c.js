a11884adbc97a9cff829a1264ad64afcc2bd5344
Roger Rengifo <rogerreng.c@gmail.com>
2019-06-05 23:35:50 -0500

    Updates

---
+++ precache-manifest.378387761dc1cbccd404ebc395bfce9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc873ae5919c75f900b490893d5caf6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3afaff03cb42eb9fb110",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "283dbb1ddc04d15c4b87",
+    "url": "/robofriends/static/css/main.9bf7b273.chunk.css"
+  },
+  {
+    "revision": "3afaff03cb42eb9fb110",
+    "url": "/robofriends/static/js/2.6d86e711.chunk.js"
+  },
+  {
+    "revision": "283dbb1ddc04d15c4b87",
+    "url": "/robofriends/static/js/main.5ccf165a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
